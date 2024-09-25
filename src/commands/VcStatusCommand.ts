a9ca3f05b55d2b050bd5c8b0dde0676e85f3d68c
Kamesuta <kamesuta@gmail.com>
2024-09-25 10:09:22 +0900

    管理者は他のVCいじれるように

--- src/commands/VcStatusCommand.ts
+++ src/commands/VcStatusCommand.ts
@@ -1,5 +1,6 @@
 import {
   ChatInputCommandInteraction,
+  PermissionFlagsBits,
   RepliableInteraction,
   SlashCommandSubcommandBuilder,
 } from 'discord.js';
@@ -48,59 +49,73 @@ class VcStatusCommand extends SubcommandInteraction {
   ): Promise<void> {
     // メンバーを取得
     const member = await interaction.guild?.members.fetch(interaction.user.id);
+    if (!member) {
+      await interaction.reply({
+        ephemeral: true,
+        content: 'メンバー情報が取得できませんでした',
+      });
+      return;
+    }
 
     // コマンドを打っているチャンネルを取得
     const commandChannel = interaction.channel;
-    if (!commandChannel) {
+    if (!commandChannel?.isVoiceBased()) {
       await interaction.reply({
         ephemeral: true,
-        content: 'コマンドを打っているチャンネルが取得できませんでした',
+        content: 'VCチャンネルでコマンドを実行してください',
       });
       return;
     }
+
     // 入っているVCチャンネルを取得
-    const vcChannel = member?.voice.channel;
-    if (!vcChannel) {
-      await interaction.reply({
-        ephemeral: true,
-        content: `<#${commandChannel.id}> に入ってからコマンドを実行してください`,
-      });
-      return;
-    }
+    const vcChannel = member.voice.channel;
 
-    // コマンドを打っているチャンネルがコマンドのVCチャンネルでない場合
-    if (commandChannel.id !== vcChannel.id) {
-      await interaction.reply({
-        ephemeral: true,
-        content: `このコマンドは <#${vcChannel.id}> で実行してください`,
-      });
-      return;
-    }
+    // 管理者(チャンネル管理)は無条件で許可
+    if (!member.permissions.has(PermissionFlagsBits.ManageChannels)) {
+      // 入っているVCチャンネルをチェック
+      if (!vcChannel) {
+        await interaction.reply({
+          ephemeral: true,
+          content: `<#${commandChannel.id}> に入ってからコマンドを実行してください`,
+        });
+        return;
+      }
 
-    // 権限をチェック
-    if (
-      !member
-        .permissionsIn(vcChannel)
-        .has(1n << 48n /* SetVoiceChannelStatus */)
-    ) {
-      await interaction.reply({
-        ephemeral: true,
-        content: 'チャンネルステータスを設定する権限がありません',
-      });
-      return;
+      // コマンドを打っているチャンネルがコマンドのVCチャンネルでない場合
+      if (commandChannel.id !== vcChannel.id) {
+        await interaction.reply({
+          ephemeral: true,
+          content: `このコマンドは <#${vcChannel.id}> で実行してください`,
+        });
+        return;
+      }
+
+      // 権限をチェック
+      if (
+        !member
+          .permissionsIn(vcChannel)
+          .has(1n << 48n /* SetVoiceChannelStatus */)
+      ) {
+        await interaction.reply({
+          ephemeral: true,
+          content: 'チャンネルステータスを設定する権限がありません',
+        });
+        return;
+      }
     }
 
     // メッセージを設定
-    await setVoiceStatus(vcChannel, message);
+    await setVoiceStatus(commandChannel, message);
 
     // メッセージを送信
     await interaction.reply({
+      ephemeral: commandChannel.id !== vcChannel?.id,
       content: `<@${interaction.user.id}> がチャンネルステータスを「${message}」に設定しました`,
       allowedMentions: { users: [] },
     });
 
     // イベントを呼び出し
-    await onVoiceStatusUpdate(vcChannel.id, message);
+    await onVoiceStatusUpdate(commandChannel.id, message);
   }
 }
 
